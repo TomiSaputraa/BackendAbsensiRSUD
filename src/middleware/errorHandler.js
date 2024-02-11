0731f309f3c8ec676bbce6423de86fc9dc62622d
Tomi Saputra <tommycodingjj@gmail.com>
2024-02-11 20:48:41 +0700

    update error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,59 +1,7 @@
 require("dotenv").config();
 
-// const errorHandler = (err, req, res, next) => {
-//   const statusCode = res.statusCode ? res.statusCode : 500;
-
-//   switch (statusCode) {
-//     case constants.VALIDATION_ERROR:
-//       res.json({
-//         title: "Validation Failed",
-//         message: err.message,
-//         stackTrace: err.stack,
-//       });
-//       break;
-
-//     case constants.NOT_FOUND:
-//       res.json({
-//         title: "Not Found",
-//         message: err.message,
-//         stackTrace: err.stack,
-//       });
-//       break;
-
-//     case constants.UNAUTHORIZED:
-//       res.json({
-//         title: "Unauthorized",
-//         message: err.message,
-//         stackTrace: err.stack,
-//       });
-//       break;
-
-//     case constants.FORBIDDEN:
-//       res.json({
-//         title: "Forbidden",
-//         message: err.message,
-//         stackTrace: err.stack,
-//       });
-//       break;
-
-//     case constants.SERVER_ERROR:
-//       res.json({
-//         title: "Server Error",
-//         message: err.message,
-//         stackTrace: err.stack,
-//       });
-//       break;
-//     default:
-//       console.log("No error all good");
-//       break;
-//   }
-// };
-
-// module.exports = {
-//   errorHandler,
-// };
-
-// ErrorHandler.js
+// dont delete next!
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   console.log("Middleware Error Handling");
   const errStatus = res.statusCode || 500;
